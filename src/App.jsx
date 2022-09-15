065f927b6597558a40aaf49adbc9743cc25d3434
Dawid-Dwk <dawiddrzewinski@gmail.com>
2022-09-15 22:27:18 +0200

    feat: added-about

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,13 @@ import React from "react";
 import "./App.css";
 import Footer from "./modules/Footer";
 import Interests from "./modules/Interests";
+import About from "./modules/About";
 
 function App() {
   return (
     <div className="App">
       <div className="main">
+        <About />
         <Interests />
         <Footer />
       </div>
---
+++ src/modules/About.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function About() {
+  return (
+    <div className="about">
+      <h2 className="about-tile">About</h2>
+      <p className="about-content">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua.
+      </p>
+    </div>
+  );
+}
