dab576835d14fb5364c669e77f3ab34ec6498126
Dawid-Dwk <dawiddrzewinski@gmail.com>
2022-09-21 22:10:05 +0200

    feat: added-anchor css-update

--- src/modules/Info.jsx
+++ src/modules/Info.jsx
@@ -5,7 +5,7 @@ export default function Info() {
   const fullName = "Dawid Drzewiński";
   const job = "Frontend Developer";
   const website = "dawiddrzewinski.website";
-  const iconSize = 16;
+  const iconSize = 14;
   const fotoURL =
     "https://media-exp1.licdn.com/dms/image/C4E03AQE10m7SVjlTHA/profile-displayphoto-shrink_200_200/0/1604337353673?e=1669248000&v=beta&t=ycDeeIqGiBxTQcOqFyqUT4tqI7LaI_yEUXKNXqCIDfs";
 
@@ -22,21 +22,32 @@ export default function Info() {
         <h3 className="info-website">{website}</h3>
       </a>
       <div className="info-buttons">
-        <button className="info--email-button" type="button">
-          <Mail className="info--email-icon" color="black" size={iconSize} />
-          Email
-        </button>
-        <button className="info--linkedin-button" type="button">
-          <Linkedin
-            onClick={
-              "location.href='www.linkedin.com/in/dawid-drzewiński-0028961ba'"
-            }
-            className="info--linkedin-icon"
-            color="#5093E2"
-            size={iconSize}
-          />
-          LinkedIn
-        </button>
+        <a
+          className="info--mail-href"
+          href="https://mail.google.com/mail/u/0/?fs=1&to=dawid.drzewinski@gmail.com&su=Contact+via+a+digital+business+card&body=Hi%20Dawid,&tf=cm"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <button className="info--mail-button" type="button">
+            <Mail className="info--mail-icon" color="black" size={iconSize} />
+            Email
+          </button>
+        </a>
+        <a
+          className="info--linkedin-href"
+          href="https://www.linkedin.com/in/dawid-drzewiński-0028961ba"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <button className="info--linkedin-button" type="button">
+            <Linkedin
+              className="info--linkedin-icon"
+              color="#5093E2"
+              size={iconSize}
+            />
+            <p>LinkedIn</p>
+          </button>
+        </a>
       </div>
     </div>
   );
